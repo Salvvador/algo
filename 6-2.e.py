cc06c0e93ee4f21ad138a14ab8ac53b93fb1adba
Salvador <pspomykaslki@gmail.com>
2019-08-05 19:48:43 +0200

    6-2.e

--- 6-2.e.py
+++ 6-2.e.py
@@ -5,9 +5,10 @@ def d_arry_get_parent(d, i):
     return math.floor((i - 1) / d)
 
 
-def d_arry_heap_insert(arr, d, key):
-    arr.append(key)
-    i = len(arr) - 1
+def d_arry_heap_increase_key(arr, d, i, key):
+    if arr[i] > key:
+        return
+    arr[i] = key
     parent = d_arry_get_parent(d, i)
     while parent >= 0 and arr[parent] < arr[i]:
         arr[parent], arr[i] = arr[i], arr[parent]
@@ -16,5 +17,5 @@ def d_arry_heap_insert(arr, d, key):
 
 
 arr0 = [19, 17, 18, 11, 10, 6, 9, 16, 10, 7, 5, 2]
-d_arry_heap_insert(arr0, 3, 20)
-print(arr0)  # expected: 20, 17, 18, 19, 10, 6, 9, 16, 10, 7, 5, 2, 11
+d_arry_heap_increase_key(arr0, 3, 3, 20)
+print(arr0)  # expected: 20, 17, 18, 19, 10, 6, 9, 16, 10, 7, 5, 2

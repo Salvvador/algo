1839fbeda8fd59567d1cee6a8c212c87245b0888
Salvador <pspomykaslki@gmail.com>
2019-08-15 18:11:18 +0200

    quick sort

---
+++ quicksort.py
@@ -0,0 +1,29 @@
+def quickSort(arr, left=0, right=None):
+    if right is None:
+        right = len(arr) - 1
+    if left >= right:
+        return arr
+    p = partition(arr, left, right)
+    quickSort(arr, left, p - 1)
+    quickSort(arr, p + 1, right)
+    return arr
+
+
+def partition(arr, left, right):
+    pivot = arr[right]
+    j = left
+    for i in range(left, right):
+        if arr[i] <= pivot:
+            arr[i], arr[j] = arr[j], arr[i]
+            j += 1
+    arr[right], arr[j] = arr[j], arr[right]
+    return j
+
+
+arr1 = [3, 12, 43, 1, 32, 11, 11, 3, 4, 12]
+arr2 = [4]
+arr3 = [5, 4, 3, 2, 1]
+
+print(quickSort(arr1))
+print(quickSort(arr2))
+print(quickSort(arr3))
